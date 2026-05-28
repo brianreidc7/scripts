dc083c2fb77fbf368f446825340b9525494a14d7
Brian Reid <31985319+brianreidc7@users.noreply.github.com>
2026-05-28 17:10:57 +0100

    LegDN v2 for 2026

--- Convert-LegacyDNToX500.ps1
+++ Convert-LegacyDNToX500.ps1
@@ -1,5 +1,6 @@
-$Addr= Read-Host "Enter full IMCEAEX address"
+$Addr= Read-Host "Enter full IMCEAEX/EX address in the email NDR"
 $Repl= @(@("_","/"), @("\+20"," "), @("\+28","("), @("\+29",")"), @("\+2C",","), @("\+5F", "_" ), @("\+40", "@" ), @("\+2E", "." ))
-$Repl | ForEach { $Addr= $Addr -replace $_[0], $_[1] }
+$Repl | ForEach-Object { $Addr= $Addr -replace $_[0], $_[1] }
+$Addr= "$Addr" -replace "EX:","" -replace "@.*$", ""
 $Addr= "X500:$Addr" -replace "IMCEAEX-","" -replace "@.*$", ""
 Write-Host $Addr
